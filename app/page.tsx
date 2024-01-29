6587153919a0d63777d713d1d4ea4a6e2f9f23d4
Krinesh <pkrinesh95@gmail.com>
2024-01-29 23:32:06 +0530

    feat: added assignment link

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,13 @@ export default function Home() {
           Full Stack Developer
         </p>
         <p className="max-w-xl text-pretty font-mono text-secondary-foreground/80">
-          I have successfully completed all three assignments with fully responsive design. I took
-          the liberty to introduce a custom dark and light mode inspired by Daisy UI. Additionally,
-          I added a floating navbar that hides on mouse leave and is initially hidden for the{' '}
+          I have successfully completed all three{' '}
+          <span className="text-foreground cursor-pointer underline transition-all hover:underline-offset-2">
+            <Link href="https://inxtest.netlify.app/">assignments</Link>
+          </span>{' '}
+          with fully responsive design. I took the liberty to introduce a custom dark and light mode
+          inspired by Daisy UI. Additionally, I added a floating navbar that hides on mouse leave
+          and is initially hidden for the{' '}
           <span className="text-foreground cursor-pointer underline transition-all hover:underline-offset-2">
             <Link href="/center-div">Center the Div</Link>
           </span>{' '}
